1e95bca39eb46123a54e0b22f0e384bd84b036b3
Joey Leung <joeyleung8@gmail.com>
2017-05-18 22:40:06 -0700

    add redis

--- server.js
+++ server.js
@@ -5,6 +5,18 @@ const app = express();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
 
+const redis = require('redis');
+const redisClient = redis.createClient({host: 'localhost', port: 6379});
+
+redisClient.on('ready', function() {
+  console.log("Redis is ready");
+});
+
+redisClient.on('error', function() {
+  console.log("Error is Redis");
+});
+
+
 app.set('port', (process.env.PORT || 3001));
 
 app.use(express.static(__dirname + '/node_modules'));
